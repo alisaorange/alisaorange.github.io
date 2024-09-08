ac8cd95ed1f381a71616f7a9b4d2e070c51aa16d
alisaorange <kolosovaalisa@gmail.com>
2024-09-08 18:38:39 +0300

    add info

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,9 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <ul>
+          <li>Научиться делать фронтэнд с помощью ReactJS</li>
+          <li>Овладеть технологиями typescript и остальным необходимым для разработки на ReactJS</li>
+          <li>PHP, HTML, CSS, composer, Nginx, yii2</li>
+          <li>3 года работаю бэкэнд разработчиком на PHP, иногда делаю правки на фронте в HTML/CSS.</li>
+          <li>Люблю котов</li>
+        </ul>
       </header>
     </div>
   );
