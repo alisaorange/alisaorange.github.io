8500ebfabab539426dc36edbff5b6045a7bf3751
AlisaOrange <kolosovaalisa@gmail.com>
2024-11-07 21:11:02 +0300

    hw8 на примере Collapse сделала правный вывод и скрытие модального окна

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useLayoutEffect, useReducer, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import './modal.css';
 
@@ -8,13 +8,68 @@ export interface ModalProps {
     children: React.ReactNode;
 }
 
-export const Modal: React.FC<ModalProps> = ({ visible, onClose, children }) => {
-    if (!visible) return null;
+type ModalState = {
+    mounted: boolean;
+    visible: boolean;
+};
 
-    // Используем портал для монтирования модального окна в body
+enum ModalActionType {
+    show,
+    hide,
+    mount,
+    unmount,
+}
+
+const reducer = (state: ModalState, action: ModalActionType): ModalState => {
+    switch (action) {
+        case ModalActionType.show:
+            return { ...state, visible: true, mounted: true };
+        case ModalActionType.hide:
+            return { ...state, visible: false };
+        case ModalActionType.mount:
+            return { ...state, mounted: true };
+        case ModalActionType.unmount:
+            return { mounted: false, visible: false };
+        default:
+            return state;
+    }
+};
+
+export const Modal: React.FC<ModalProps> = ({ visible, onClose, children }) => {
+
+    const [state, dispatch] = useReducer(reducer, { visible: false, mounted: false });
+    const modalRef = useRef<HTMLDivElement>(null);
+    const modalContentRef = useRef<HTMLDivElement>(null);
+
+    const handleTransitionEnd = (e: React.TransitionEvent) => {
+        if (e.target !== e.currentTarget) return;
+        if (!state.visible) {
+            dispatch(ModalActionType.unmount);
+        }
+    };
+
+    useLayoutEffect(() => {
+        if (visible) {
+            dispatch(ModalActionType.mount);
+            setTimeout(() => dispatch(ModalActionType.show), 1);
+        } else {
+            dispatch(ModalActionType.hide);
+        }
+    }, [visible]);
+
+    if (!state.mounted) return null;
+
     return ReactDOM.createPortal(
-        <div className="modal-overlay" onClick={onClose}>
-            <div className="modal-window" onClick={(e) => e.stopPropagation()}>
+        <div
+            className={`modal-overlay ${state.visible ? 'modal-overlay--open' : 'modal-overlay--close'}`}
+            onClick={onClose}
+        >
+            <div
+                ref={modalContentRef}
+                className={`modal-window ${state.visible ? 'modal-window--open' : 'modal-window--close'}`}
+                onClick={(e) => e.stopPropagation()}
+                onTransitionEnd={handleTransitionEnd}
+            >
                 <button className="modal-close-button" onClick={onClose}>
                     ×
                 </button>
