285af8f483b068b2195653976f5317f01c7fb6e5
AlisaOrange <kolosovaalisa@gmail.com>
2024-11-10 17:32:30 +0300

    hw7 обернула в мемо меню и кнопку

--- src/shared/navBar/NavBar.tsx
+++ src/shared/navBar/NavBar.tsx
@@ -10,15 +10,19 @@ interface NavBarProps {
     links: NavLink[];
 }
 
+const NavLinkItem: React.FC<NavLink> = React.memo(({ label, href }) => (
+    <a href={href} className="nav-link">
+        {label}
+    </a>
+));
+
 const NavBar: React.FC<NavBarProps> = ({ links }) => {
     return (
         <nav className="nav">
             <span className="nav-links">
-                {links.map((link, index) => (
-                    <a key={index} href={link.href} className="nav-link">
-                        {link.label}
-                    </a>
-                ))}
+               {links.map((link, index) => (
+                   <NavLinkItem key={index} label={link.label} href={link.href} />
+               ))}
             </span>
         </nav>
     );
--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, memo } from 'react';
 import cn from 'clsx';
 import './button.css';
 
@@ -9,17 +9,19 @@ interface ButtonProps {
   onClick?: () => void;
 }
 
-export const Button: FC<ButtonProps> = ({ type, size, label, onClick, ...props}) => {
-  const mode = type  ? 'storybook-button--type-two' : 'storybook-button--type-one';
+const Button: FC<ButtonProps> = ({ type, size = 'medium', label, onClick, ...props }) => {
+    const mode = type ? 'storybook-button--type-two' : 'storybook-button--type-one';
 
-  return (
-      <button
-          type="button"
-          className={cn('storybook-button', `storybook-button--${size}`, mode)}
-          onClick={onClick}
-          {...props}
-      >
-        {label}
-      </button>
-  );
+    return (
+        <button
+            type="button"
+            className={cn('storybook-button', `storybook-button--${size}`, mode)}
+            onClick={onClick}
+            {...props}
+        >
+            {label}
+        </button>
+    );
 };
+
+export default memo(Button);
